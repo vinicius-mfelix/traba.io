0a54c17b94130dd36ea1b4ecb39d5cee8698abfa
vinicius-mfelix <vinicius.mfelix@icloud.com>
2021-07-09 16:29:22 -0300

    Added Application Time Zone to UTC.

--- src/main/java/com/growth/startupone/StartupOneApplication.java
+++ src/main/java/com/growth/startupone/StartupOneApplication.java
@@ -1,5 +1,7 @@
 package com.growth.startupone;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartupOneApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(StartupOneApplication.class, args);
 	}
 
