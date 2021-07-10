cd14d59fc3818d748ce69625df7dd4d2e0bd4013
vinicius-mfelix <vinicius.mfelix@icloud.com>
2021-07-10 01:15:57 -0300

    Added Maven failsafe plugin to integration tests.

---
+++ src/test/java/com/growth/startupone/StartuponeApplicationIT.java
@@ -0,0 +1,13 @@
+package com.growth.startupone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartuponeApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
