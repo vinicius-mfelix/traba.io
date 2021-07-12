92c10f2a6746e15cdf7bd421615c7db372ec9ea1
vinicius-mfelix <vinicius.mfelix@icloud.com>
2021-07-12 11:27:22 -0300

    Swagger URI resource remapping test.

--- src/main/java/com/growth/startupone/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/growth/startupone/core/openapi/SpringFoxConfig.java
@@ -133,7 +133,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("swagger-ui.html")
 			.addResourceLocations("classpath:/META-INF/resources/");
 		
-		registry.addResourceHandler("/webjars/**")
+		registry.addResourceHandler("v1/webjars/**")
 			.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
