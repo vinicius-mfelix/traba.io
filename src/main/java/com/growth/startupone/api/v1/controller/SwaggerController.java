e6b8ff8c41b046c7c2bff0f426f3b0345f518aa4
vinicius-mfelix <vinicius.mfelix@icloud.com>
2021-07-13 15:31:14 -0300

    Added API Ignore annotation to Swagger redirect controller.

--- src/main/java/com/growth/startupone/api/v1/controller/SwaggerController.java
+++ src/main/java/com/growth/startupone/api/v1/controller/SwaggerController.java
@@ -3,6 +3,9 @@ package com.growth.startupone.api.v1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import springfox.documentation.annotations.ApiIgnore;
+
+@ApiIgnore
 @Controller
 public class SwaggerController {
 
